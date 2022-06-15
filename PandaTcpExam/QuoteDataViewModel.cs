81ff271cd04d0b6d3530f5af753e5f617de832ad
Raz Megrelidze <rezomegrelidze@gmail.com>
2022-06-15 22:07:33 +0300

    some code cleanup

---
+++ PandaTcpExam/QuoteDataViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PandaTcpExam.Common;
+
+namespace PandaTcpExam;
+
+public class QuoteDataViewModel
+{
+    public static QuoteDataViewModel FromQuoteData(QuoteData data)
+    {
+        return new QuoteDataViewModel
+        {
+            AssetName = data.Symbol,
+            Bid = data.Bid,
+            Ask = data.Ask,
+            Time = new DateTime(data.TickTime).ToString("dd/MM/yyyy-HH:mm:ss.fff")
+        };
+    }
+
+    public string Time { get; set; }
+
+    public decimal Ask { get; set; }
+
+    public decimal Bid { get; set; }
+
+    public string AssetName { get; set; }
+}
---
+++ PandaTcpExam/QuotesClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PandaTcpExam.Common;
+
+namespace PandaTcpExam;
+
+public class QuotesClient
+{
+    private readonly ObservableCollection<QuoteDataViewModel> _collection;
+    private readonly Dictionary<string, int> _symbolIndex;
+
+    public QuotesClient(ObservableCollection<QuoteDataViewModel> collection,Dictionary<string,int> symbolIndex)
+    {
+        _collection = collection;
+        _symbolIndex = symbolIndex;
+    }
+
+    public async Task StartReading()
+    {
+        await OpenTcpStream();
+    }
+
+    private async Task OpenTcpStream()
+    {
+        var tcpClient = new TcpClient();
+        tcpClient.Connect("localhost", 50000);
+
+        using (var n = tcpClient.GetStream())
+        using (var reader = new StreamReader(n))
+        {
+            while (!reader.EndOfStream)
+            {
+                string json = await reader.ReadLineAsync();
+                if (string.IsNullOrEmpty(json))
+                {
+                    throw new Exception("Server error!");
+                }
+                QuoteData data = JsonConvert.DeserializeObject<QuoteData>(json);
+
+                if (_symbolIndex.TryGetValue(data.Symbol, out int index))
+                {
+                    _collection[index] = QuoteDataViewModel.FromQuoteData(data);
+                }
+                else
+                {
+                    _collection.Add(QuoteDataViewModel.FromQuoteData(data));
+                    _symbolIndex[data.Symbol] = _collection.Count - 1;
+                }
+            }
+        }
+    }
+}
